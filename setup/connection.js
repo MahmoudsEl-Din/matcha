2a67469758fd030d1a30a3192de343ce7d4d6776
Nicolas Terol <nterol@student.42.fr>
2018-01-23 19:43:54 +0100

    fake data import finished

--- setup/connection.js
+++ setup/connection.js
@@ -1,5 +1,3 @@
-// import { v4 } from 'public-ip'
-
 class init_db {
     constructor() {
         this.mysql = require('mysql')
@@ -36,6 +34,9 @@ class init_db {
             + 'pop INT, CHECK (pop BETWEEN 0 AND 100)'
             + ');', (error) => {
                 if (error) throw error
+                let table = 'users'
+                let file = 'setup/users.csv'
+                dontLeaveMeEmpty(table, file)
         })
       
         this.connection.query('CREATE TABLE IF NOT EXISTS code('
@@ -53,6 +54,7 @@ class init_db {
         + 'tag_name VARCHAR(250) NOT NULL'
         + ');', (error) => {
             if (error) throw error
+            dontLeaveMeEmpty('tags', 'setup/tags.csv')
         })
 
         this.connection.query('CREATE TABLE IF NOT EXISTS tag_list('
@@ -60,6 +62,7 @@ class init_db {
         + 'tag_name VARCHAR(250) NOT NULL'
         + ');', (error) => {
             if (error) throw error
+            dontLeaveMeEmpty('tag_list', 'setup/tag_list.csv')
         })
 
         this.connection.query('CREATE TABLE IF NOT EXISTS pictures('
@@ -102,34 +105,23 @@ class init_db {
         + 'uid_target INT NOT NULL'
         + ');', (error) => {
             if (error) throw error
+            dontLeaveMeEmpty('likes', 'setup/likes.csv')
         })
 
-        // function dontLeaveMeEmpty (table, file) {
-        //     let sql = "SELECT * FROM ?;"
-        //     thi.connection.query(sql, [table], (error, results) => {
-        //         if (error) throw error
-        //         if (results.length === 0) {
-                   
-        //         }
-        //     })
-        // }
-
-        let sql = "SELECT * FROM users;"
-        this.connection.query(sql, (error, results) => {
-            if (error) throw error
-            if (results.length === 0) {
-                // Larmina : Larmina777 JeanMiche : Test1234 s.foret2mailinator rAtOnLAVEUR888 lucie@mailinator.com Lulu123456789  botozo78@mailinator.com Lolcat78 cheval.love@mailinator.com ChevalAmour6
-                sql = "LOAD DATA LOCAL INFILE '~/http/MyWebSite/matcha/setup/users.csv' INTO TABLE users FIELDS TERMINATED BY ',' ENCLOSED BY '\"'LINES TERMINATED BY '\n' IGNORE 1 ROWS;"
-                this.connection.query(sql, (error) => {
-                    if (error) throw error
-                })
-                sql = "INSERT INTO `tag_list` (`tag_name`) VALUES ('sexy'), ('cheum'), ('bardot'), ('hashtag'), ('boucherie')" +
-                ";" 
-                this.connection.query(sql, (error) => {
+        function dontLeaveMeEmpty (table, file) {
+            let sql = "SELECT * FROM " +table+" ;"
+            connection.query(sql, (error, results) => {
                 if (error) throw error
+                if (results.length === 0) {
+                    sql = "LOAD DATA LOCAL INFILE '"+file+"' INTO TABLE "+table+" FIELDS TERMINATED BY ',' ENCLOSED BY '\"'LINES TERMINATED BY '\n' IGNORE 1 ROWS;"
+                    connection.query(sql, [file, table], (error) => {
+                        if (error) throw error
+                        else
+                        console.log("filling :" + table)
+                    })
+                }
             })
-            }
-        })
+        }
         global.connection = this.connection
     }
 }
